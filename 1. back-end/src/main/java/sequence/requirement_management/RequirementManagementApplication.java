3641d1eeb1e43391c5ec7087b42b0fadb136c902
Daeil-Jung <wjdeodlf0123@gmail.com>
2018-10-01 17:24:14 +0900

    resetting_backend

---
+++ 1. back-end/src/main/java/sequence/requirement_management/RequirementManagementApplication.java
@@ -0,0 +1,12 @@
+package sequence.requirement_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirementManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RequirementManagementApplication.class, args);
+    }
+}
