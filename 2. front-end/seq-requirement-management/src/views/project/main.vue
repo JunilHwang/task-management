31e507fa42f026ec21bbbe43b60b3e8c65059c91
Eun-young <bey4314@naver.com>
2018-10-09 01:20:04 +0900

    최근조회프로젝트 슬라이드 하는중

--- 2. front-end/seq-requirement-management/src/views/project/main.vue
+++ 2. front-end/seq-requirement-management/src/views/project/main.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="project-main">
-
     <h3 class="content-title">프로젝트 관리</h3>
     <div class="float-wrap">
       <section class="section float-wrap">
@@ -19,21 +18,26 @@
         <p v-else>즐겨찾기 된 목록이 없습니다</p>
       </section>
 
-      <section class="section float-wrap">
+      <section class="section float-wrap viewd">
         <h4 class="section-title">최근에 조회한 프로젝트</h4>
         <template v-if="projectList.length">
-          <article v-for="(data, key) in viewedProject" :key="key" v-if="date- (data.viewDate/1000).toFixed(0) < (60*60*24*7)">
-            <div @click.prevent="projectView(data.writer, data.uri, data.idx)">
-              <p class="article-title" v-html="data.subject" />
-              <p class="description" v-html="data.description" />
-              <p class="date" v-html="getDateFormat(data.date)" />
-              <p class="date" v-html="getDateFormat(data.viewDate)" />
+          <div class="view-box">
+            <div class="viewed-ul">
+              <article class="viewed-li" v-for="(data, key) in viewedProject" :key="key" v-if="date-(data.viewDate/1000).toFixed(0) < (60*60*24*7)">
+                <div @click.prevent="projectView(data.writer, data.uri, data.idx)">
+                  <p class="article-title" v-html="data.subject" />
+                  <p class="description" v-html="data.description" />
+                  <p class="date" v-html="getDateFormat(data.date)" />
+                </div>
+                <i :class="data.star==1 ? 'star' : null" 
+                @click.prevent="icon(data.idx, data.star)" class="color far fa-star animated fadeInRight"></i>
+              </article>
             </div>
-            <i :class="data.star==1 ? 'star' : null" 
-            @click.prevent="icon(data.idx, data.star)" class="color far fa-star animated fadeInRight"></i>
-          </article>
+          </div>
         </template>
         <p v-else>최근에 조회한 프로젝트 목록이 없습니다</p>
+        <i @click="previousSlide" class="fas fa-angle-left left"></i>
+        <i @click="nextSlide" class="fas fa-angle-right right"></i>
       </section>
 
       <section class="section float-wrap">
@@ -44,7 +48,6 @@
               <p class="article-title" v-html="data.subject" />
               <p class="description" v-html="data.description" />
               <p class="date" v-html="getDateFormat(data.date)" />
-              <p class="date" v-html="getDateFormat(data.viewDate)" />
             </div>
             <i :class="data.star==1 ? 'star' : null" 
             @click.prevent="icon(data.idx, data.star)" class="color far fa-star animated fadeInRight"></i>
@@ -97,12 +100,13 @@
         implementList: [],
         testList: [],
         date: (+new Date()/1000).toFixed(0),
-        limit: 3
+        viewCount: 0
       }
     },
+
     computed: {
       viewedProject: function () {
-        return _.orderBy(this.projectList, 'viewDate', 'desc').slice(0, 3)
+        return _.orderBy(this.projectList, 'viewDate', 'desc')
       }
     },
     methods: {
@@ -120,6 +124,16 @@
         }
         Api.putStar({idx, star})
         this.projectList = (await Api.getProjectListOfMain(this.$store.state.member.idx)).rows
+      },
+      previousSlide () {
+
+      },
+      nextSlide () {
+
+      },
+      viewCnt (key) {
+        const keyd = key
+        console.log(keyd)
       }
     }
   }
@@ -135,12 +149,19 @@
 .description{font-size:17px;margin-bottom:5px;height:20px;}
 .date{font-size:13px;color:#aaa}
 article{position: relative;background:#fff;padding:20px;border-radius:3px;border:1px solid #ddd;box-shadow:0 0 10px fade-out(#666, .8);transition:.3s;cursor:pointer;width:calc(33.3333% - 20px);float:left;box-sizing:border-box;margin:0 10px;
-  &:hover{box-shadow:0 0 10px fade-out(#666, .3);};
-  &>i {display: none; color: #FFCC00}
-  &:hover>i {position: absolute; display: block; font-size: 20px; right: 5%; bottom :10%; padding: 5px;};
-  &>i:hover {
-    font-weight: bold
-  }
+  &:hover{box-shadow:0 0 10px fade-out(#666, .3);}
+  &>i {display: none; color: #FFCC00;}
+  &:hover>i {position: absolute; display: block; font-size: 20px; right: 5%; bottom :10%; padding: 5px;}
+  &>i:hover {font-weight: bold; }
 }
 .star { font-weight: bold}
+.viewd {position: relative; overflow: hidden;
+  &>i { position: absolute; cursor:pointer; font-size: 50px; color: #bbb; top:calc(50% - 5px); }
+}
+.left { left: 1%}
+.right { right: 1%}
+
+.viewed-ul { display: flex;   }
+.viewed-li { }
+.view-box { overflow: hidden;}
 </style>
